ef1f821fcdc3cbcca812c4543f4d64e4c4246158
Gustavo Franke <franke.gustavo@gmail.com>
2020-11-30 08:57:50 +0000

    run all tests from main

--- test/Tests.hs
+++ test/Tests.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
+import Test.QuickCheck
+import System.Exit
+
+runTests :: IO Bool
+runTests = $quickCheckAll
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  good <- and <$> sequence [runTests]
+  if good
+     then exitSuccess
+     else exitFailure
